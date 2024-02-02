a816e378b1323b19b7242334465003bc625ab207
JosselinSomervilleRoberts <josselin.somerville@gmail.com>
2024-02-01 16:32:32 -0800

    Add Gemfile creation/editing

--- main.py
+++ main.py
@@ -26,7 +26,34 @@ def search_github_repos(max_size_kb: int, limits: int = 100):
         return []
 
 
+def clone_repo(repo_url: str, download_path: str, repo_name: str):
+    os.system(f"git clone {repo_url} {download_path}/{repo_name}")
+
+
+def setup_gemfile(repo_path: str):
+    # Check if Gemfile exists, if not, copy Gemfile.default to Gemfile
+    if not os.path.exists(f"{repo_path}/Gemfile"):
+        os.system(f"cp Gemfile.default {repo_path}/Gemfile")
+        return
+
+    # Gemfile exists, check if it has the jekyll gem
+    if "jekyll" in open(f"{repo_path}/Gemfile").read():
+        # TODO: figure out if we need to do anything here
+        return
+
+    # Gemfile exists, but doesn't have jekyll gem
+    with open(f"{repo_path}/Gemfile", "a") as file:
+        file.write('gem "jekyll", "~> 4.3.3"')
+
+
+def serve_repo(repo_path: str):
+    setup_gemfile(repo_path)
+    os.system(f"cd {repo_path} && bundle install && bundle exec jekyll serve")
+
+
 repos = search_github_repos(10, limits=10)
 
-for repo in repos:
-    print(repo["full_name"], repo["html_url"])
+for i, repo in enumerate(repos):
+    print(f"{i+1}. {repo['full_name']} - {repo['html_url']}")
+    clone_repo(repo["html_url"], "repos", f"{i}_{repo['name']}")
+    serve_repo(f"repos/{i}_{repo['name']}")
