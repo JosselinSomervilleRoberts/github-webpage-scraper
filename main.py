fd325215b21aeba04900b3c5819835fa74de28ff
JosselinSomervilleRoberts <josselin.somerville@gmail.com>
2024-02-06 17:19:18 -0800

    main now uses new Jekyll server

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def main():
         created_after=datetime.datetime(2021, 1, 1),
         language="HTML",
         max_size_kb=1000,
-        limits=10,
+        limits=50,
         verbose=True,
     )
 
@@ -25,18 +25,17 @@ def main():
         print("\n" + "=" * 50)
         repo_name = f"{i}_{repo['name']}"
         clone_url = repo["clone_url"]
-        port = 4000  # + i
+        port = 4000
         print(f"Cloning {clone_url} to {path}/{repo_name}")
         clone_repo(clone_url, path, repo_name)
 
         # Start the Jekyll server
         server = JekyllServer(f"{path}/{repo_name}", verbose=True)
-        server.start()
+        success: bool = server.start()
 
-        # Sleep to let the server start
-        time_sleeping: int = 10
-        print(f"Sleeping for {time_sleeping} seconds to let the server start...")
-        time.sleep(time_sleeping)
+        if not success:
+            print(f"Failed to start the server for {repo_name}. Skipping...")
+            continue
 
         # Take a screenshot of a random page
         try:
@@ -53,7 +52,6 @@ def main():
 
         # Stop the Jekyll server
         server.stop()
-        time.sleep(2)
 
         # Delete the repository
         # print(f"Deleting {path}/{repo_name}")
